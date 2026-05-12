0614a2ecac3291281ba9be43338b242e72a80240
Dmytro Farbun <dimofarb@gmail.com>
2026-05-12 19:07:17 +0300

    feat: initialize mini request web app with React, Vite, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-[impact] text-3xl text-center mt-[20vh] text-blue-500 hover:text-blue-600">
+        Mini Request System
+      </h1>
+    </>
+  );
+}
+
+export default App;
