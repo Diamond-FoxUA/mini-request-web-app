392ad694996d9b0f4a5b162fbba1da373f8f4951
Dmytro Farbun <dimofarb@gmail.com>
2026-05-12 21:52:06 +0300

    feat: add RequestList component to display user and manager requests based on role

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import Header from "./widgets/Header";
 import SelectRoleModal from "./features/SelectRoleModal/SelectRoleModal";
 import { useState } from "react";
+import { useAppStore } from "./store/useAppStore";
+import RequestList from "./features/RequestList/RequestList";
 
 function App() {
+  const { role } = useAppStore();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <main className="max-w-6xl my-6 flex flex-col gap-10 mx-auto">
       <Header onOpenModal={() => setIsModalOpen(true)} />
+
       {isModalOpen && <SelectRoleModal onClose={() => setIsModalOpen(false)} />}
+
+      {role === "user" && (
+        <RequestList />
+      )}
     </main>
   );
 }
